bd565cfe2e4004e1a49c29cbb07e63e6644c8178
Soule222 <50902734+Soule222@users.noreply.github.com>
2022-04-05 13:36:10 -0400

    Update index.js
    Added additional functionality to my loading animation. It now loads THEN shows the results form, instead of it being automatically loaded up.
    
    Also added a bit of functionality to allow the results page to be hidden in case of a second calculation a user wants to do.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 calcButton = document.querySelector('#calculate');
 calcButton.addEventListener('click', calculate);
+results = document.querySelector('.results');
 
 function calculate(e) {
     e.preventDefault();
+
+    // a check to see if  previous results need to be hidden (if a clalculation has already been made)
+
+    if(results.classList.contains('hide')){
+        console.log('hidden');
+    } else {
+        results.classList.add('hide');
+    };
+    
+    // Getting all of the information from the form to be able to use in the formula for loan calculation
 
     principal = Number(loanAmount.form.value);
     interestRate = (Number(apr.form.value))/100;
@@ -23,22 +34,18 @@ function calculate(e) {
 
     totalInt = totalAcc - principal
 
-    if (typeof(principal) === 'number' & typeof(interestRate)  === 'number' & typeof(timeYears)  === 'number'){
-        
-        results = document.querySelector('.results')
-        results.classList.remove('hide');
-        
-    } else {
-        console.log('please check your information inputs');
-    };
+    // This is the function for creating the loading graphic
 
     loader('on');
+
     displayResults(totalAcc, monthlyPay, totalInt);
 };
 
+    // This function will show / hide a loading graphic while results are calculated
+
 function loader(toggle){
     if (toggle === 'on'){
-        loading = document.querySelector('.loader');
+        loading = document.querySelector('.loader');    
         loading.classList.remove('hide');
     } else if (toggle === 'off'){
         loading.classList.add('hide');
@@ -49,8 +56,21 @@ const sleep = (milliseconds) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
   }
 
+  // This function takes all of the information provided in the form and places it into a results box.
+  // The sleep function makes it take a few seconds and also allows for the form to behave correctly with materialze animations
+
 function displayResults(totalAcc, monthlyPay, totalInt){
-    sleep(5000).then(() => {
+    sleep(3000).then(() => {
+
+        if (typeof(principal) === 'number' & typeof(interestRate)  === 'number' & typeof(timeYears)  === 'number'){
+        
+            results = document.querySelector('.results')
+            results.classList.remove('hide');
+            
+        } else {
+            console.log('please check your information inputs');
+        };
+    
         loader('off');
         monthlyPayment.form.focus();
         monthlyPayment.form.value= `$ ${monthlyPay}`
@@ -62,6 +82,9 @@ function displayResults(totalAcc, monthlyPay, totalInt){
  
 
 }
+
+// This is a class constructor that will get all the elements I need. Should be cleaned up,
+// Just another way of selecting elements. 
 
 class formBox {
 
@@ -98,3 +121,7 @@ let loanLength = new formBox('time');
 let monthlyPayment = new formBox('monthly-payment');
 let totalPayment = new formBox('total-payment');
 let totalInterest = new formBox('total-interest');
+
+
+
+console.log(results.classList);
